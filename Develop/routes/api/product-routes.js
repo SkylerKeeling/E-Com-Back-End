fbcb6e077545923b180b7c12a91ad9dabfb61857
skyler <skyler@skylers-MacBook-Pro.local>
2023-09-12 10:57:03 -0300

    bug fixed in product tag bullk create function

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router()
+const {Router} = require("express")
 const {Product, Category, Tag, ProductTag} = require("../../models")
 
 // The `/api/products` endpoint
@@ -38,27 +39,28 @@ router.post("/", (req, res) => {
       "tagIds": [1, 2, 3, 4]
     }
   */
-Product.create(req.body)
-  .then(product => {
-    // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-    if (req.body.tagIds.length) {
-      const productTagIdArr = req.body.tagIds.map(tag_id => {
-        return {
-          product_id: product.id,
-          tag_id,
-        }
-      })
-      return ProductTag.bulkCreate(productTagIdArr)
-    }
-    // if no product tags, just respond
-    res.status(200).json(product)
-  })
-  .then(productTagIds => res.status(200).json(productTagIds))
-  .catch(err => {
-    console.log(err)
-    res.status(400).json(err)
-  })
-
+router.post("/", (req, res) => {
+  Product.create(req.body)
+    .then(product => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map(tag_id => {
+          return {
+            product_id: product.id,
+            tag_id,
+          }
+        })
+        return ProductTag.bulkCreate(productTagIdArr)
+      }
+      // if no product tags, just respond
+      res.status(200).json(product)
+    })
+    .then(productTagIds => res.status(200).json(productTagIds))
+    .catch(err => {
+      console.log(err)
+      res.status(400).json(err)
+    })
+})
 // update product
 router.put("/:id", (req, res) => {
   // update product data
